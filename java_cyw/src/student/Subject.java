68fd6bb9fb9dd22cf5099ee7105412124bb69f70
btywoo <btywoo17@gmail.com>
2024-06-27 17:49:20 +0900

    06.27

--- java_cyw/src/student/Subject.java
+++ java_cyw/src/student/Subject.java
@@ -1,5 +1,7 @@
 package student;
 
+import java.util.Objects;
+
 import lombok.Data;
 
 @Data
@@ -8,5 +10,24 @@ public class Subject {
 	private int grade, semester, midterm, finalExam, perAss;
 	
 	private String name;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Subject other = (Subject) obj;
+		return grade == other.grade && midterm == other.midterm && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(grade, midterm, name);
+	}
+	
+	
 
 }
