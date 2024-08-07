fca661b493bbceb7698e11027aae63816d96103f
btywoo <btywoo17@gmail.com>
2024-08-07 16:37:32 +0900

    servlet 수업

---
+++ servlet/src/main/java/servlet1/model/vo/Person.java
@@ -0,0 +1,13 @@
+package servlet1.model.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Person {
+	
+	private String name;
+	private int age;
+
+}
